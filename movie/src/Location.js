fccc0291ebc789d4b45a7a66f0ae325c9c071641
EngHyu <roomedia@naver.com>
2019-11-30 12:52:30 +0000

    refactor Location.js

--- movie/src/Location.js
+++ movie/src/Location.js
@@ -14,21 +14,15 @@ class Location extends Component {
 
   constructor() {
     super()
+    const make_area = (ele) => Array(ele['count']).fill(<Area name={ele['name']}/>)
+    const make_wrapper = (ele) => <Wrapper array={make_area(ele)} onClick={() => this.get_detail(ele['id'])}/>
+    
     axios.get(area)
-    .then(res => res.data.map(
-        ele => 
-        <Wrapper
-          array={Array(ele['count']).fill(<Area name={ele['name']} />)}
-          onClick={() => this.get_detail(ele['id'])}
-        />
-      )
-    )
-    .then(list => this.setState({...this.state, list: list }))
-    .catch(err => {
-      console.log(err);
-    })
-    .finally(() => {
-    });
+    .then(res => res.data)
+    .then(data => data.map(ele => make_wrapper(ele)))
+    .then(wrapper => this.setState({...this.state, list: wrapper }))
+    .catch(err => console.log(err))
+    .finally(() => console.log('finish Location constructor', area))
   }
 
   render() {
@@ -36,25 +30,22 @@ class Location extends Component {
   }
 
   get_detail(id) {
+    console.log('start Location get_detail', id)
+    const make_url = (data) => theater + data['cd']
+    const make_arr = (arr, ele) => arr.concat(ele.data['theaCdList'])
+    const make_area = (ele) => <Area name={ele['cdNm']} onClick={() => this.select(ele['cd'])}/>
+
     axios.get(detail + id)
-    .then(res => res.data['basareaCdList'].map(
-      data => axios.get(theater + data['cd'])
-    ))
+    .then(res => res.data['basareaCdList'])
+    .then(arr => arr.map(data => make_url(data)))
+    .then(arr => arr.map(url => axios.get(url)))
     .then(arr => axios.all(arr))
-    .then(res => res.reduce(
-      (arr, ele) => arr.concat(ele.data['theaCdList']), [])
-    )
-    .then(arr =>
-      <Wrapper
-        array={arr.map(
-          ele=><Area
-            name={ele['cdNm']}
-            onClick={() => this.select(ele['cd'])}
-          />
-        )}
-      />
-    )
-    .then(list => this.setState({...this.state, 'detail': list }))
+    .then(res => res.reduce((arr, ele) => make_arr(arr, ele), []))
+    .then(arr => arr.map(ele => make_area(ele)))
+    .then(areas => <Wrapper array={areas}/>)
+    .then(wrappers => this.setState({...this.state, detail: wrappers }))
+    .catch(err => console.log(err))
+    .finally(() => console.log('finish Location get_detail', id))
   }
 
   select(id) {
@@ -64,17 +55,13 @@ class Location extends Component {
 
 class Area extends Component {
   render() {
-    return (
-      <a onClick={this.props.onClick}>{this.props.name}</a>
-    );
+    return (<a onClick={this.props.onClick}>{this.props.name}</a>)
   }
 }
 
 class Wrapper extends Component {
   render() {
-    return (
-      <div onClick={this.props.onClick}>{this.props.array}</div>
-    );
+    return (<div onClick={this.props.onClick}>{this.props.array}</div>)
   }
 }
 

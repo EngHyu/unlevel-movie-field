185420d414896961d9bbc61ef1e83ca252ee9ea4
EngHyu <roomedia@naver.com>
2019-12-14 13:22:42 +0000

    fix path for api

--- movie/src/Const.js
+++ movie/src/Const.js
@@ -1,7 +1,8 @@
 const BASE = 'http://127.0.0.1:6006/api/'
 const URL = {
     CITY: BASE + 'city',
-    TOWN: BASE + 'town',
+    TOWN: BASE + 'town/',
+    THEATER: BASE + 'theater/',
     MOVIE: BASE + 'movie',
 }
 
